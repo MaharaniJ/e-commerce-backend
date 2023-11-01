dc6cd1f93d5e4fac2c73b8cb614f0ceb9cdbe6e1
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-11-01 13:36:45 +0530

    auth

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,35 +2,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userSchema");
 const secretKey = process.env.SECRET_KEY;
 
-// const authenticate = async (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization;
-//     console.log(token);
-//     const verifyToken = jwt.verify(token, secretKey);
-//     console.log(verifyToken);
-
-//     const rootUser = await User.findOne({
-//       _id: verifyToken._id,
-//       "tokens.token": token,
-//     });
-
-//     if (!rootUser) {
-//       throw new Error("User Not Found");
-//     }
-
-//     req.token = token;
-//     req.rootUser = rootUser;
-//     req.userID = rootUser._id;
-
-//     next();
-//   } catch (error) {
-//     res.status(401).send("Unauthorized:No token provided");
-//     console.log(error);
-//   }
-// };
-
-// module.exports = authenticate;
-
 let authenticate1 = async function (req, res, next) {
   console.log("header MEssage", req.headers);
   if (req.headers.authorization) {
